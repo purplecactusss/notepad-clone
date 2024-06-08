dda9fb8a1ea146803fafd148f9024f19658599d4
purplecactusss <chettinger@ucsd.edu>
2024-06-07 21:02:48 -0700

    frogsjs2

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const editor = document.getElementById('editor');
-    const preFilledText = "
-              _         _
-  __   ___.--'_`.     .'_`--.___   __
- ( _`.'. -   'o` )   ( 'o`   - .`.'_ )
- _\.'_'      _.-'     `-._      `_`./_
+    const preFilledText = `
+               _         _
+  __   ___.--'_\`.     .'\`_--.___   __
+ ( _\`.'. -   'o\` )   ( 'o\`   - .\`.'_ )
+ _\\.'_'      _.-'     \`-._      \`_\`./_
 ( \`. )    //\`         '/\\    ( .'/ )
- \_`-'`---'\\__,       ,__//`---'`-'_/
-  \`        `-\         /-'        '/
-   `                               '";
+ \\_\`-'`---'\\\\__,       ,__//\`---'\`-'_/
+  \\\`        \`-\         /-'        '/
+   \`                               '
+   `; // Multiline string containing the ASCII art
     editor.textContent = preFilledText; // Use textContent to set non-editable text
 });
