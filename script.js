9672c39020650ef59f57a69f664dae3cab07dbee
purplecactusss <chettinger@ucsd.edu>
2024-06-07 20:07:52 -0700

    add initial files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const editor = document.getElementById('editor');
+    const preFilledText = "This is the pre-filled text. You can edit this as needed.";
+    editor.value = preFilledText;
+});
