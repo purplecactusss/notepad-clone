c55e702bf833000d037ede6a36137b4993872a45
purplecactusss <chettinger@ucsd.edu>
2024-06-07 22:07:50 -0700

    rwac2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const editor = document.getElementById('editor');
-    const preFilledText = "06.11.2024 22:00 RWAC 0115 south entrance";
+    const preFilledText = `06.11.2024 22:00 RWAC 0115 south entrance`;
     editor.textContent = preFilledText; // Use textContent to set non-editable text
 });
