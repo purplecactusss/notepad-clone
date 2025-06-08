4c52b0e2ad18aaa69d745fe75ceb8641703d7b74
purplecactusss <chettinger@ucsd.edu>
2025-06-07 20:54:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const editor = document.getElementById('editor');
-    const preFilledText = `06.10.2024 22:00\n32.88033° N, 117.24109° W`;
+    const preFilledText = `🦩 06.10.2024 22:00 🦩\n🦩 32.88033° N, 117.24109° W 🦩`;
     editor.textContent = preFilledText; // Use textContent to set non-editable text
 });
